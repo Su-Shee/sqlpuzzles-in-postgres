bd887edb95906b9cb8a9425a6f68857028b83048
Susanne Schmidt <sushee@outlook.com>
2013-08-08 14:27:04 +0200

    naming scheme cleanup

---
+++ absentees/t/01-absentees.sql
@@ -0,0 +1,18 @@
+-- pg_prove -U postgres -d sqlpuzzles 01-absentees-tables.sql --verbose
+-- psql -U postgres -h 10.13.2.47 -d sqlpuzzles -Xf 01-absentees-tables.sql
+
+BEGIN;
+
+SELECT plan(5);
+
+SELECT has_table('personell');
+SELECT has_pk('personell');
+
+SELECT has_column('personell', 'employee_id');
+
+SELECT col_not_null('personell', 'employee_id');
+
+SELECT col_type_is('personell', 'employee_id', 'integer');
+
+SELECT * FROM finish();
+ROLLBACK;
---
+++ absentees/t/02-absentees.sql
@@ -0,0 +1,27 @@
+-- pg_prove -U postgres -d sqlpuzzles 02-absentees-tables.sql --verbose
+-- psql -U postgres -h 10.13.2.47 -d sqlpuzzles -Xf 02-absentees-tables.sql
+
+BEGIN;
+
+SELECT plan(14);
+
+SELECT has_table('absence');
+SELECT has_pk('absence');
+
+SELECT has_column('absence', 'reason');
+SELECT has_column('absence', 'absence_date');
+SELECT has_column('absence', 'severity');
+SELECT has_column('absence', 'employee');
+
+SELECT col_not_null('absence', 'reason');
+SELECT col_not_null('absence', 'absence_date');
+SELECT col_not_null('absence', 'severity');
+SELECT col_not_null('absence', 'employee');
+
+SELECT col_type_is('absence', 'reason', 'text');
+SELECT col_type_is('absence', 'absence_date', 'date');
+SELECT col_type_is('absence', 'severity', 'smallint');
+SELECT col_type_is('absence', 'employee', 'integer');
+
+SELECT * FROM finish();
+ROLLBACK;
