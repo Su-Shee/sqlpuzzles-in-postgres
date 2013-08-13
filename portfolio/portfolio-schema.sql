c34feaada088b0bc897bbdd6650ed7175ebc799e
Susanne Schmidt <sushee@outlook.com>
2013-08-13 14:43:07 +0200

    removed code which didnt belong

--- portfolio/portfolio-schema.sql
+++ portfolio/portfolio-schema.sql
@@ -14,23 +14,3 @@ CREATE TABLE succession (
   PRIMARY KEY (chain, next)
 );
 
---CREATE TRIGGER badge_status AFTER INSERT OR UPDATE ON badges
---FOR EACH ROW EXECUTE PROCEDURE invalidate_badge();
---
---CREATE OR REPLACE FUNCTION invalidate_badge() RETURNS TRIGGER AS
---$$ 
---BEGIN
---  UPDATE badges SET status = 'I' WHERE NEW.employee = employee AND issued < NEW.issued;
-----    FROM (SELECT B1.employee, B1.status, B1.issued 
-----      FROM (SELECT employee, issued, status 
-----            FROM badges 
-----            WHERE employee = NEW.employee 
-----            EXCEPT 
-----            SELECT employee, MAX(issued), status FROM badges GROUP BY employee, status) 
-----            AS B1) 
-----      AS B2 WHERE B2.employee = badges.employee AND B2.issued = badges.issued;
---  RETURN NEW;
---END; 
---$$ 
---LANGUAGE plpgsql;
---
